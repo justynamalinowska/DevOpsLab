7ff3e64c8e8950c06a48d2907e6b883c3b052d3f
justyna <justynamalinowska758@gmail.com>
2024-11-08 13:04:20 +0100

    update button

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           eyes={person.eyes}
         />
       ))}
-      <button class="button" onclick='http://localhost:8080/'>Go to Backend</button>
+      <button class="button" onclick="window.location.href='http://localhost:8080/'">Go to Backend</button>
     </div>
   );
 }
